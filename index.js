9cc84fec4fd9c875d3f2a29c5bcc0f7f6a2d053c
Cogsy <beecher.braedan@gmail.com>
2024-05-18 11:23:23 -0600

    changed the height to match the acceptance criteria

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ inquirer.prompt(questions).then(answers => {
     }
 
     const svgContent = `
-<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
   ${shape.render()}
   <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
 </svg>`;
